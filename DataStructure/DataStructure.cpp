d7ea5c10b698b570c98de2dcb0dd12c7e13e56da
ivar1990 <ivar.m.juarez@gmail.com>
2017-07-12 16:38:16 -0600

    reduce some commented lines

--- DataStructure/DataStructure.cpp
+++ DataStructure/DataStructure.cpp
@@ -11,56 +11,7 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	//LinkedList<int> list;
-
-	//list.Add(2);
-	//list.Add(3);
-	//list.Add(4);
-
-	//for (int i = 0; i < 5; i++)
-	//{
-	//	list.Add(i);
-	//}
-
-	//list.printList();
-	////cout << "Amount of Nodes Deleted: " << list.removeAllNodes(3) << endl
-	//if (list.FindNode(2,4))
-	//{
-	//	cout << "Found node value: " << list.search_node->data << " at position: " << list.search_node->position << endl;
-	//}
-	//else
-	//{
-	//	cout << "could not find value" << endl;
-	//}
-	//list.printList();
-
-	//list.Move(5, 7);
-	//cout << "Moving:........." << endl;
-
-	//list.printList();
-	//list.Connect(2, 2);
-	//list.Connect(4, 1);
-	//list.Connect(2, 1);
-	//list.Connect(3, 2);
-	//list.Connect(2, 4);
-
-	//list.ShowConnections();
-
-	//list.FindNode(2);
-
-	//list.GetConnections(list.search_node);
-
-	//list.PrintNodeConnections();
-
-	//list.MoveConnection(2, 4);
-
-	//list.ShowConnections();
 	
-
-	//Connector connector;
-	//connector.Connect(119, 114);
-
-	//connector.ShowConnections();
 
 	//Reference Table
 	//1 = number
@@ -147,36 +98,6 @@ int _tmain(int argc, _TCHAR* argv[])
 	//assembler.OutputStructure(&structure3);
 	*/
 	
-	
-	//NodeSystem node_system;
-	//Connector connector(&node_system);
-	
-	//Abstractor abstractor;
-
-	//abstractor.ConstructRandomStructure();
-	
-
-	/*for (int i = 0; i < 256; i++)
-	{
-		node_system.Add(i);
-		cout << i << ": " << static_cast<char>(i) << endl;
-
-	}
-
-	node_system.printList();
-
-	node_system.FindNode(87);
-
-	cout << "Node: " << node_system.search_node << endl;*/
-
-
-	//connector.Connect(98, 114);
-	////connector.Connect(114, 98);
-	//connector.Connect(98, 119);
-	//connector.Connect(114, 119);
-	//connector.Connect(119, 114);
-	//connector.Connect(119, 98);
-
 
 	/*Abstractor abstractor(&node_system, &connector);
 
